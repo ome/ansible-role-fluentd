4ec9ee20460a4a8ae1c3224a881e714751a9a74e
Simon Li <spli@dundee.ac.uk>
2018-01-03 16:37:12 +0000

    Test fluentd with nginx

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,4 +1,6 @@
 import testinfra.utils.ansible_runner
+import json
+import time
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
@@ -8,3 +10,26 @@ def test_service_running_and_enabled(Service):
     service = Service('td-agent')
     assert service.is_running
     assert service.is_enabled
+
+
+def test_nginx(Command, File):
+    Command("curl -sf http://localhost/nonexistent.html")
+    time.sleep(10)
+
+    txt = File('/var/log/td-agent/nginx/fluentd.log').content_string
+    lines = txt.splitlines()
+    access = lines[-2].split(None, 2)
+    error = lines[-1].split(None, 2)
+
+    assert access[1] == 'nginx.access'
+    accessj = json.loads(access[2])
+    assert accessj['host'] == '127.0.0.1'
+    assert accessj['method'] == 'GET'
+    assert accessj['path'] == '/nonexistent.html'
+    assert accessj['code'] == '404'
+    assert 'curl' in accessj['agent']
+
+    assert error[1] == 'nginx.error'
+    errorj = json.loads(error[2])
+    assert errorj['log_level'] == 'error'
+    assert 'No such file or directory' in errorj['message']
