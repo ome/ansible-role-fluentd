94dcb1b9e68643276d1910300880df24158c22e8
Sebastien Besson <seb.besson@gmail.com>
2019-10-19 23:07:08 +0100

    Upgrade role tests to Molecule 2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,25 +1,30 @@
-import testinfra.utils.ansible_runner
+import os
 import json
 import time
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_service_running_and_enabled(Service):
-    service = Service('td-agent')
+def test_service_running_and_enabled(host):
+    service = host.service('td-agent')
     assert service.is_running
     assert service.is_enabled
 
 
-def test_nginx(Command, File):
-    Command("curl -sf http://localhost/nonexistent.html")
+def test_nginx(host):
+    host.command("curl -sf http://localhost/nonexistent.html")
     time.sleep(10)
 
-    access = File(
+    access = host.file(
         '/var/log/td-agent/nginx-access/fluentd.log'
         ).content_string.splitlines()[-1].split(None, 2)
-    error = File(
+    error = host.file(
         '/var/log/td-agent/nginx-error/fluentd.log'
         ).content_string.splitlines()[-1].split(None, 2)
 
